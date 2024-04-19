21678a38d251dc6a83c306d1ea45f991df976bd8
Baiano1407 <127527442+Baiano1407@users.noreply.github.com>
2024-04-18 21:36:35 -0300

    jk

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ const tempoObjetivo3 = new Date("2023-12-30T00:00:00");
 const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
 const tempoObjetivo5 = new Date("2024-02-01T00:00:00");
 
-const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
+const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4,tempoObjetivo5];
 
 
 function calculaTempo(tempoObjetivo) {
